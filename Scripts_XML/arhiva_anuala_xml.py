c92e342546cd3fe8ad8d59bcd079a7b38e3596c6
mihairadulescu-ux <mihai.radulescu@legomax.org>
2026-07-24 12:23:51 +0300

    Update batch size and pause duration settings
    Increased batch size and pause duration for processing.

--- Scripts_XML/arhiva_anuala_xml.py
+++ Scripts_XML/arhiva_anuala_xml.py
@@ -21,8 +21,8 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from drive_config import FOLDERE_XML_IDS
 
-DIMENSIUNE_BATCH = 100
-PAUZA_SECUENTE_SEC = 2.5
+DIMENSIUNE_BATCH = 200
+PAUZA_SECUENTE_SEC = 4
 
 # Citire An Filtru din Argumente (ex: python redenumeste_fisiere_drive.py 1992)
 AN_FILTRU = None
